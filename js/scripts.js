27012dda7edaeaacb0aa4d71218eb932a5bee8f5
Robin Nowakowski <rn222hk@student.lnu.se>
2016-11-24 12:54:04 +0100

    added SEO stuff and trying some js to fix scrollbar messing up width of page

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+/**
+ * Created by Robin on 2016-11-24.
+ */
+// This script fixes the shift that occurs in a centered layout
+// when the page grows and forces scrollbars to appear.
+// http://aaubry.net/fixing-jumping-content-when-scrollbars-appear.html
+$(function () {
+    var body = $("body");
+
+    var previousWidth = null;
+
+    // Function that applies padding to the body
+    // to adjust its position.
+    var resizeBody = function () {
+        var currentWidth = body.width();
+        if (currentWidth !== previousWidth) {
+            previousWidth = currentWidth;
+
+            // Measure the scrollbar size
+            body.css("overflow", "hidden");
+            var scrollBarWidth = body.width() - currentWidth;
+            body.css("overflow", "auto");
+
+            body.css("margin-left", scrollBarWidth + "px");
+        }
+    };
+
+    // setInterval is required because the resize event
+    // is not fired when a scrollbar appears or disappears.
+    setInterval(resizeBody, 100);
+    resizeBody();
+});
